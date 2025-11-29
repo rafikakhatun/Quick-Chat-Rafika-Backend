104ea619b3eb7874c64fa734b9b5695bb7bde248
rafika khatun <rafikakhatun607@gmail.com>
2025-11-29 21:11:19 +0530

    refactor: update MongoDB connection to use async/await

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectDB = () => {
+const connectDB = async () => {
     try {
-        const conn = mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected:${conn.connection.host}`)
 
     } catch (error) {
