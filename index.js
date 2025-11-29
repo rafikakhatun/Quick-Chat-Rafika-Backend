298ec56840cbaee149ccf1ada7e4580a6b409cc3
rafika khatun <rafikakhatun607@gmail.com>
2025-11-29 21:13:33 +0530

    feat: call connectDB in server to initialize MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+// mongoDB connection
 
+
+// DB connection call
+connectDB()
 
 const PORT = process.env.PORT || 5001;
 
