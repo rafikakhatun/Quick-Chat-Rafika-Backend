e1ef8f40fb729971ccf46f0bea6679cbdbdf377f
rafika khatun <rafikakhatun607@gmail.com>
2025-12-01 21:43:01 +0530

    Add POST route for creating a new user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const express = require('express');
+const { createUser } = require('../controllers/user.controllers');
 const router = express.Router();
 
-
+router.post('/create', createUser); // POST API ROUTE
 
 
 module.exports = router;
